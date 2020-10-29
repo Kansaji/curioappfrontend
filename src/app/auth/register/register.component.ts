56dbd538c05c3f4413a79edcb04efcb6750af1c0
Kansaji <lakruwan45@gmail.com>
2020-10-29 19:15:18 +0530

    create account form client side validation,custom password,email validators applied

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { RegisterPaylord } from '../register-paylord';
 import { AuthService } from '../auth.service';
 
@@ -12,14 +12,19 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
   registerPaylord: RegisterPaylord;
+  
 
   constructor(private formBuilder:FormBuilder, private authService:AuthService) {
     this.registerForm = this.formBuilder.group({
-      username:'',
-      email:'',
-      telephone:'',
-      password:''
+      username:['',[Validators.required, Validators.minLength(4)]],
+      email:['',[Validators.required, this.emailValidator]],
+      telephone:['',[Validators.required, Validators.minLength(9)]],
+      password:['',[Validators.required, Validators.minLength(8)]],
+      confirmPassword:['',[Validators.required, this.passwordValidator]]
     });
+    this.registerForm.controls.password.valueChanges.subscribe(
+      x => this.registerForm.controls.confirmPassword.updateValueAndValidity()
+    )
 
     this.registerPaylord={
       username:'',
@@ -27,21 +32,56 @@ export class RegisterComponent implements OnInit {
       telephone:'',
       password:'',
     }
+    
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
+    this.registerForm.markAllAsTouched();
     this.registerPaylord.username = this.registerForm.get('username').value;
     this.registerPaylord.email = this.registerForm.get('email').value;
     this.registerPaylord.telephone = this.registerForm.get('telephone').value;
     this.registerPaylord.password = this.registerForm.get('password').value;
-    console.log(this.registerPaylord);
-    this.authService.register(this.registerPaylord).subscribe(data=>{
-      console.log("register success");
-    },error=>{
-      console.log("register failed");
-    });
+   
+    if(this.registerForm.valid){
+      console.log(this.registerPaylord);
+      this.authService.register(this.registerPaylord).subscribe(data=>{
+        console.log("register success");
+      },error=>{
+        console.log("register failed");
+      });
+    }else{
+      
+    }
+  }
+
+  emailValidator(control: AbstractControl){
+    if(control && (control.value!==null || control.value!== undefined)){
+      const regex = new RegExp('^[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}$');
+      if(!regex.test(control.value)){
+        return{
+          isError:true
+        };
+      }
+    }
+    return null;
+  }
+
+  passwordValidator(control: AbstractControl){
+    if(control && (control.value!==null || control.value!== undefined)){
+      const cnfpassValue = control.value;
+      const passControl = control.root.get('password')
+      if(passControl){
+        const pssValue = passControl.value;
+        if(pssValue !== cnfpassValue || pssValue===''){
+          return{
+            isError: true
+          }
+        }
+      }
+    }
+    return null;
   }
 }
